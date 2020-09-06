c447d1e1df6d025c1dd74ab0ad897eb60fc3c377
Artur Teben <teben.artur99@gmail.com>
2020-09-06 19:38:44 +0300

    Create enum with genres

---
+++ src/main/java/com/cursor/teben/patterns/builder/MovieGenre.java
@@ -0,0 +1,14 @@
+package com.cursor.teben.patterns.builder;
+
+public enum MovieGenre {
+    ADVENTURE,
+    COMEDY,
+    CRIME,
+    DRAMA,
+    FANTASY,
+    HISTORICAL,
+    HORROR,
+    ROMANCE,
+    THRILLER,
+    WESTERN
+}
