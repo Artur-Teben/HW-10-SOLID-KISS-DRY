697c0e2e5fca97a6686722fbfc0b6774aa9aa7c8
Artur Teben <teben.artur99@gmail.com>
2020-09-06 19:35:40 +0300

    Call method run() for start program

---
+++ src/main/java/com/cursor/teben/Main.java
@@ -0,0 +1,10 @@
+package com.cursor.teben;
+
+import com.cursor.teben.executor.Executor;
+
+public class Main {
+    public static void main(String[] args) {
+        Executor executor = new Executor();
+        executor.run();
+    }
+}
