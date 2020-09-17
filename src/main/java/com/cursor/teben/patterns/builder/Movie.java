79f7bdcdd49999c9c6aca4820568c79bbb3a64ad
Artur Teben <teben.artur99@gmail.com>
2020-09-17 16:25:42 +0300

    Make MovieBuilder nested in Movie

--- src/main/java/com/cursor/teben/patterns/builder/Movie.java
+++ src/main/java/com/cursor/teben/patterns/builder/Movie.java
@@ -1,9 +1,16 @@
 package com.cursor.teben.patterns.builder;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
 public class Movie {
     private final String name;
     private final List<MovieGenre> genre;
@@ -13,17 +20,6 @@ public class Movie {
     private final Integer ageRestrictions;
     private final String language;
     private final BigDecimal budget;
-
-    Movie(final MovieBuilder movieBuilder) {
-        this.name = movieBuilder.getName();
-        this.genre = movieBuilder.getGenre();
-        this.country = movieBuilder.getCountry();
-        this.duration = movieBuilder.getDuration();
-        this.premiereDate = movieBuilder.getPremiereDate();
-        this.ageRestrictions = movieBuilder.getAgeRestrictions();
-        this.language = movieBuilder.getLanguage();
-        this.budget = movieBuilder.getBudget();
-    }
 
     @Override
     public String toString() {
@@ -37,5 +33,61 @@ public class Movie {
                 ", language='" + language + '\'' +
                 ", budget=" + budget + "million" +
                 '}';
+    }
+
+    @NoArgsConstructor
+    public static class Builder {
+        private String name;
+        private List<MovieGenre> genre;
+        private String country;
+        private Integer duration;
+        private Calendar premiereDate;
+        private Integer ageRestrictions;
+        private String language;
+        private BigDecimal budget;
+
+        public Builder buildName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder buildGenre(List<MovieGenre> genre) {
+            this.genre = genre;
+            return this;
+        }
+
+        public Builder buildCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder buildDuration(Integer duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        public Builder buildPremiereDate(GregorianCalendar premiereDate) {
+            this.premiereDate = premiereDate;
+            return this;
+        }
+
+        public Builder buildAgeRestrictions(Integer ageRestrictions) {
+            this.ageRestrictions = ageRestrictions;
+            return this;
+        }
+
+        public Builder buildLanguage(String language) {
+            this.language = language;
+            return this;
+        }
+
+        public Builder buildBudget(BigDecimal budget) {
+            this.budget = budget;
+            return this;
+        }
+
+        public Movie build() {
+            return new Movie(name, genre, country, duration, premiereDate, ageRestrictions, language, budget);
+        }
     }
 }
